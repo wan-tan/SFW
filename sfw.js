5f6731e6ba44d7b7816c3a320366f12b9c6f6376
Pierre Humberdroz <p.humberdroz@gmail.com>
2016-09-04 14:11:46 +0200

    Added: CUID for unique ID generation getTags (loads tags that are saved in the Chrome storage) addTag (adds a tag to the chrome storage under sfw_tags) removeTag (removes a tag based on the ID)
    Updated:
    delete history function to look for tags there are saved in the chrome storage
    Manifest.json

--- sfw.js
+++ sfw.js
@@ -1,5 +1,42 @@
 function deletehistory() {
-    var domain = { "text": "freelancer" }
-    chrome.history.search(domain, function(history) { history.map(function(item, index) { console.log(item.url);
-            chrome.history.deleteUrl({ "url": item.url }) }) })
+    getTags(function(test) {
+        test.map(function(item) {
+            var search = { 'text': item.tag }
+            chrome.history.search(search, function(history) {
+                history.map(function(item, index) {
+                    chrome.history.deleteUrl({ "url": item.url })
+                })
+            })
+        })
+    })
+}
+
+function getTags(cb) {
+    chrome.storage.sync.get('sfw_tags', function(obj) {
+        cb(obj.sfw_tags)
+    })
+}
+
+function addTag(value) {
+    chrome.storage.sync.get('sfw_tags', function(obj) {
+        if (Array.isArray(obj.sfw_tags)) {
+            obj.sfw_tags.push({ 'tag': value, 'id': cuid() });
+        } else {
+            obj.sfw_tags = [{ 'tag': value, 'id': cuid().toString() }]
+        }
+        chrome.storage.sync.set({ 'sfw_tags': obj.sfw_tags });
+    })
+}
+
+function removeTag(id) {
+    chrome.storage.sync.get('sfw_tags', function(obj) {
+        sfw_tags = obj.sfw_tags.filter(function(item) {
+            if (id == item.id) {
+                return false;
+            } else {
+                return true;
+            }
+        })
+        chrome.storage.sync.set({ 'sfw_tags': sfw_tags });
+    })
 }
